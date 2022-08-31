7381f6bd6b6a50b23511935310cda80bc8f5d395
guest03 <guest03@CHEINFD10>
2022-08-30 21:30:42 -0300

    Adicionar arquivos de projeto.

---
+++ Aula30082022/Controllers/CalculadoraController.cs
@@ -0,0 +1,26 @@
+﻿using Aula30082022.Models.ViewModel;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Aula30082022.Controllers
+{
+    public class CalculadoraController : Controller
+    {
+        public IActionResult Index()
+        {
+            ViewBag.Resultado = 0;
+            return View(new ValoresCalculadora());
+        }
+
+        [HttpPost]
+        public IActionResult IndexSoma(ValoresCalculadora form)
+        {
+
+            if (form.Operacao == Operacoes.Soma)
+                form.resultado = form.Valor2 + form.Valor2;
+
+            ViewBag.Resultado = form.resultado;
+            return View(form);
+        }
+
+    }
+}
---
+++ Aula30082022/Models/ViewModel/ValoresCalculadora.cs
@@ -0,0 +1,17 @@
+﻿namespace Aula30082022.Models.ViewModel
+{
+    public class ValoresCalculadora
+    {
+        public decimal Valor1 { get; set; }
+        public decimal Valor2 { get; set; }
+        public Operacoes Operacao { get; set; }
+        public decimal? resultado { get; set; }
+    }
+    public enum Operacoes : int
+    {
+        Soma = 1,
+        Subtracao = 2,
+        Multiplicacao = 3,
+        Divisao = 4
+    }
+}
